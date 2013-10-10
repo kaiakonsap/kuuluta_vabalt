60f2c5634ca274a23fbbd4962de6a8482e30f24b
kaia.konsap <kaia.konsap@gmail.com>
2013-10-10 18:51:01 +0300

    Make insert controller, insert view  and database dump(wip)

--- doc/database.sql
+++ doc/database.sql
@@ -3,24 +3,40 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Okt 10, 2013 kell 04:28 PM
--- Serveri versioon: 5.5.32
--- PHP versioon: 5.4.19
+-- Generation Time: Oct 10, 2013 at 05:48 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Andmebaas: `kuuluta`
+-- Database: `kuuluta`
 --
-CREATE DATABASE IF NOT EXISTS `kuuluta` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `kuuluta`;
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `user`
+-- Table structure for table `ad`
+--
+
+DROP TABLE IF EXISTS `ad`;
+CREATE TABLE IF NOT EXISTS `ad` (
+  `ad_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ad_title` varchar(50) NOT NULL,
+  `ad_text` text NOT NULL,
+  `ad_mail` varchar(50) NOT NULL,
+  `ad_phone` int(10) unsigned NOT NULL,
+  `ad_category` int(10) unsigned NOT NULL,
+  `ad_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ad_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -33,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Andmete tõmmistamine tabelile `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
--- views/ads/ads_insert.php
+++ views/ads/ads_insert.php
@@ -1,1 +1,24 @@
-<p>Helloo</p>
+<form method="POST" class="well span8">
+	<div class="row">
+		<div class="span3">
+			<label>Pealkiri</label>
+			<input name="title" type="text" class="span3" placeholder="Pealkiri">
+			<label>E-mail</label>
+			<input name="mail" type="text" class="span3" placeholder="Telefon">
+			<label>Telefon</label>
+			<input name="phone" type="text" class="span3" placeholder="Telefon">
+			<label>Kategooria</label>
+			<select id="category" name="category" class="span3">
+				<option value="vehicles" selected="">Sõidukid</option>
+				<option value="property">Kinnisvara</option>
+				<option value="home">Kodu</option>
+				<option value="electronics">Elektroonika</option>
+			</select>
+		</div>
+	<div class="span5">
+		<textarea name="text" id="message" class="input-xlarge span5" rows="10"></textarea>
+		<label>Kuulutus</label>
+	</div>
+		<button type="submit" class="btn btn-primary pull-right">Send</button>
+
+</form>
