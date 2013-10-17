fec71c9ad9d21e8b598ba0b3b11951b6a221f7a1
kaia.konsap <kaia.konsap@gmail.com>
2013-10-17 13:48:53 +0300

    Add dump

--- doc/database.sql
+++ doc/database.sql
@@ -3,22 +3,22 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2013 at 05:48 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Loomise aeg: Okt 17, 2013 kell 12:43 PM
+-- Serveri versioon: 5.5.32
+-- PHP versioon: 5.4.19
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `kuuluta`
+-- Andmebaas: `kuuluta`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ad`
+-- Tabeli struktuur tabelile `ad`
 --
 
 DROP TABLE IF EXISTS `ad`;
@@ -30,13 +30,79 @@ CREATE TABLE IF NOT EXISTS `ad` (
   `ad_phone` int(10) unsigned NOT NULL,
   `ad_category` int(10) unsigned NOT NULL,
   `ad_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ad_id`)
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ad_id`),
+  KEY `ad_category` (`ad_category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tabeli struktuur tabelile `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+--
+-- Andmete tõmmistamine tabelile `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Sõidukid'),
+(2, 'Kinnisvara'),
+(3, 'Kodu'),
+(4, 'Elektroonika'),
+(5, 'Arvutid'),
+(6, 'Telefonid'),
+(7, 'Ehitus'),
+(8, 'Sport'),
+(9, 'Tutvus');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE IF NOT EXISTS `event` (
+  `event_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Andmete tõmmistamine tabelile `event`
+--
+
+INSERT INTO `event` (`event_id`, `name`) VALUES
+(1, 'Lisas'),
+(2, 'Muutis'),
+(3, 'Kustutas');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `ad_id` int(10) unsigned NOT NULL,
+  `event_id` int(10) unsigned NOT NULL,
+  KEY `event_id` (`event_id`),
+  KEY `ad_id` (`ad_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -49,9 +115,26 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- Andmete tõmmistamine tabelile `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
+
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `ad`
+--
+ALTER TABLE `ad`
+  ADD CONSTRAINT `ad_ibfk_1` FOREIGN KEY (`ad_category`) REFERENCES `category` (`category_id`);
+
+--
+-- Piirangud tabelile `log`
+--
+ALTER TABLE `log`
+  ADD CONSTRAINT `log_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`),
+  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `ad` (`ad_id`);
 SET FOREIGN_KEY_CHECKS=1;
