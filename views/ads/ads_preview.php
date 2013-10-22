ef0d0cac683be49ee5fa5dc16c97a0b7af00321f
kaidomuttik <kaido.muttik@khk.ee>
2013-10-22 11:40:26 +0300

    Add ads preview functionality

---
+++ views/ads/ads_preview.php
@@ -0,0 +1,50 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<img width="320px" alt="140x140" src="http://lorempixel.com/140/140/" />
+		</div>
+		<div class="col-md-8">
+			<div class="row-fluid">
+				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
+				</div>
+				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
+				</div>
+				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
+				</div>
+				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h1>Kuulutuse nimi</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis faucibus tortor. Cras iaculis ante sed fermentum dapibus. Ut massa lacus, dignissim in erat non, malesuada malesuada turpis. Nullam eleifend odio id magna tincidunt posuere. Sed eleifend lacus at lorem semper egestas. In nec leo mollis</p>
+				</div>
+				<div class="col-md-6">
+					<button type="button" class="btn btn-primary icon pull-center">110.00 €</button>
+					<p>Telefoni number: xxx xxx 12</p>
+					<p>Email: olenlahe@khk.ee</p>
+					<p>Peeter Piider</p>
+					<p>Asukoht: Tartu Kutsehariduskeskus</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row" style="margin-top:15px;">
+		<div class="col-md-8">
+			<button type="button" class="btn btn-info btn-xs ">Kategooria</button>
+		</div>
+		<div class="col-md-4">
+			<p>Kuulutuse lisamise kuupäev</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="btn-bar well col-lg-12">
+			<button type="button" class="btn btn-default btn-lg">Tagasi kuulutust muutma</button>
+			<button type="button" class="btn btn-default btn-lg">Lisa kuulutus</button>
+		</div>
+	</div>
+</div>
