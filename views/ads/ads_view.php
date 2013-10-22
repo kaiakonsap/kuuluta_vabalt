62cc32ed99fc9cb62aef37ec9a09380672532c68
Ivo Dubolazov <ivo.dubolazov@khk.ee>
2013-10-22 11:00:33 +0300

    ads view complete (unlinked)

--- views/ads/ads_view.php
+++ views/ads/ads_view.php
@@ -1,25 +1,42 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4">
-			<img alt="140x140" src="http://lorempixel.com/140/140/" />
+			<img width="320px" alt="140x140" src="http://lorempixel.com/140/140/" />
 		</div>
 		<div class="col-md-8">
 			<div class="row-fluid">
 				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
 				</div>
 				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
 				</div>
 				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
 				</div>
 				<div class="col-md-3">
+					<img alt="140x140" src="http://lorempixel.com/140/140/" />
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-6">
+					<h1>Kuulutuse nimi</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis faucibus tortor. Cras iaculis ante sed fermentum dapibus. Ut massa lacus, dignissim in erat non, malesuada malesuada turpis. Nullam eleifend odio id magna tincidunt posuere. Sed eleifend lacus at lorem semper egestas. In nec leo mollis</p>
 				</div>
 				<div class="col-md-6">
+					<button type="button" class="btn btn-primary icon pull-center">110.00 €</button>
+					<p>Telefoni number: xxx xxx 12</p>
+					<p>Email: olenlahe@khk.ee</p>
 				</div>
 			</div>
+		</div>
+	</div>
+	<div class="row" style="margin-top:15px;">
+		<div class="col-md-8">
+			<button type="button" class="btn btn-info btn-xs ">Kategooria</button>
+		</div>
+		<div class="col-md-4">
+			<p>Kuulutuse lisamise kuupäev</p>
 		</div>
 	</div>
 </div>
