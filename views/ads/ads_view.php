7a665cd0007a6a621a6b3538df165ddf338643fd
Ivo Dubolazov <ivo.dubolazov@khk.ee>
2013-10-22 10:26:48 +0300

    ads view

---
+++ views/ads/ads_view.php
@@ -0,0 +1,25 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<img alt="140x140" src="http://lorempixel.com/140/140/" />
+		</div>
+		<div class="col-md-8">
+			<div class="row-fluid">
+				<div class="col-md-3">
+				</div>
+				<div class="col-md-3">
+				</div>
+				<div class="col-md-3">
+				</div>
+				<div class="col-md-3">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+				</div>
+				<div class="col-md-6">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
